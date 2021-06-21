60213a2ec7c5d88981525c9be679f4d4bd7ec65e
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2021-06-21 17:10:55 +0300

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -1,4 +1,5 @@
 terraform {
+  # AWS
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,12 +9,12 @@ terraform {
 
   required_version = ">= 0.14.9"
 }
-
+# Selecting default profile
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-
+# Creation of secuirty group 
 resource "aws_security_group" "opens8080" {
   name        = "allow_8080"
   description = "Allow 8080 access the machine "
@@ -30,7 +31,7 @@ resource "aws_security_group" "opens8080" {
     Name = "TerraformRockes"
   }
 }
-
+# Creationg of instance
 resource "aws_instance" "instance" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
@@ -39,7 +40,7 @@ resource "aws_instance" "instance" {
     Name = "Created_By_Terraform!"
   }
 }
-
+# Attachment of the secuirty group 
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
   security_group_id    = aws_security_group.opens8080.id
   network_interface_id = aws_instance.instance.primary_network_interface_id 
